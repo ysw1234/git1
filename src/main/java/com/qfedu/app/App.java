66e3ecae770bac12950aad06383f0354e51b1e7b
renr <1286342532@qq.com>
2019-06-13 14:12:52 +0800

    Initial commit

---
+++ src/main/java/com/qfedu/app/App.java
@@ -0,0 +1,11 @@
+package com.qfedu.app;
+
+/**
+ * Created by Administrator on 2019/6/13 0013.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
