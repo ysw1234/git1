ce8cf1687e4e2344159615dec83772c798f4a6f9
ysw1234 <51741902+ysw1234@users.noreply.github.com>
2019-06-13 15:02:46 +0800

    11111

--- src/main/java/com/qfedu/app/App.java
+++ src/main/java/com/qfedu/app/App.java
@@ -7,6 +7,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("nihao");
+        //System.out.println("nihao");
     }
 }
